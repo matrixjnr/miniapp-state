3a3ffb60021e71a337f679eef6822a45b732dd31
John Simiyu <jonnyeinstein@gmail.com>
2022-02-19 06:21:48 +0300

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,49 @@
+Page({
+  data:{
+    x : 0
+  },
+  onLoad(query) {
+    // Page load
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  },
+  onReady() {
+    // Page loading is complete
+  },
+  onShow() {
+    // Page display
+  },
+  onHide() {
+    // Page hidden
+  },
+  onUnload() {
+    // Page is closed
+  },
+  onTitleClick() {
+    // Title clicked
+  },
+  onPullDownRefresh() {
+    // Page is pulled down
+  },
+  onReachBottom() {
+    // Page is pulled to the bottom
+  },
+  onShareAppMessage() {
+    // Back to custom sharing information
+    return {
+      title: 'My App',
+      desc: 'My App description',
+      path: 'pages/index/index',
+    };
+  },
+  clickFunction(){
+    this.setData({
+      x : this.data.x + 1
+    })
+  },
+  decFunction(){
+    this.setData({
+      x : this.data.x - 1
+    })
+  }
+
+});
